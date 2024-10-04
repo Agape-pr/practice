209ef490d017da943af9f169ab8accc464034df9
Agape-pr <mugishaagapelearning@gmail.com>
2024-10-04 05:15:28 +0000

    updated

--- pra1.py
+++ pra1.py
@@ -1,12 +1,22 @@
-try:
-    num1 = int(input("enter the first number"))
-    num2 = int(input("enter the second number"))
-    result = num1 / num2
+class exception:
+    def __init__(self,num1):
+        self.num1 = num1
+    
+    def compute(self):
+        num1 = int(input("enter the  number"))
+       
+       
 
-    if num2 == 0 :
-        raise ZeroDivisionError
-    else:
-        print(f"the results = {result}")
-except ZeroDivisionError:
-    print("you can not divide with zero")
+       
+
+class ValueTooHigh(exception):
+    def __init__(self, num1):
+        super().__init__(num1)
+    try:
+        if num1 > 100: 
+            raise ValueTooHighError as e
+        else :
+            print("the  number is below 100")
+    except ValueTooHighError:
+        print("you can not divide with zero")
     
